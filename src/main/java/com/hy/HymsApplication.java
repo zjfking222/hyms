ee82fc4ead2bf089d94855858942cab9f7be300d
zjfki <zjfking222@163.com>
2018-04-09 15:49:03 +0800

    hyms spring boot

---
+++ src/main/java/com/hy/HymsApplication.java
@@ -0,0 +1,12 @@
+package com.hy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HymsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HymsApplication.class, args);
+	}
+}
