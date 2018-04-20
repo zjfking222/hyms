b448e9113a824ca381f9c5d9badeb4c7645fb95f
bunny <179346696@qq.com>
2018-04-20 16:12:09 +0800

    18/4/20

--- src/main/resources/static/js/qzgz/admin/dashboard.js
+++ src/main/resources/static/js/qzgz/admin/dashboard.js
@@ -9,4 +9,26 @@ $(function() {
         $($(this).attr('data-id')).fadeIn(0).siblings('.subpage').fadeOut(0);
     });
 
+
+
+    var dataSource = FetchData({page:1,number:1},'POST','/getCanteen',false);
+    var data = dataSource.data.canteens[0];
+    console.log(data);
+    new Vue({
+        el:'#app',
+        data: data
+    })
 });
+var FetchData = function (data, method, param, async) {
+    var response =
+        $.ajax({
+            async: async,
+            url: param,
+            type: method,
+            dataType: 'json',
+            data: data,
+            success: function (dataSource) {
+                return dataSource;
+            }});
+    return response.responseJSON;
+};
