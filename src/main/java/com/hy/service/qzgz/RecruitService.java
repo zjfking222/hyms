f75af8ad72aaf30c2dfa15c0ba32516745f77e62
bunny <179346696@qq.com>
2018-05-07 10:04:49 +0800

    18/5/7

--- src/main/java/com/hy/service/qzgz/RecruitService.java
+++ src/main/java/com/hy/service/qzgz/RecruitService.java
@@ -1,8 +1,10 @@
 package com.hy.service.qzgz;
 
+import com.hy.dto.RecruitDto;
 import com.hy.dto.RecruitWithTotalPageDto;
 
 public interface RecruitService {
     RecruitWithTotalPageDto getRecruit(int page, int number);
     Integer getTotalPageOfRecruit(int number);
+    boolean addRecruit(RecruitDto recruitDto);
 }
