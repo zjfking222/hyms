f528355566930ca11a10cc44c5af2cde2aabe3cd
bunny <179346696@qq.com>
2018-05-11 08:45:50 +0800

    18/5/11

--- src/main/resources/static/js/qzgz/web/bc.js
+++ src/main/resources/static/js/qzgz/web/bc.js
@@ -1,22 +1,4 @@
-// $(document).ready(function () {
-//     var dataSource = FetchData(null, 'POST','/getBus',false);
-//     if(dataSource.code === 0){
-//         for (var i = 0; i < dataSource.data.length; i++)
-//         {
-//             var list_item0 = list_item
-//                 .replace("#checi#",dataSource.data[i].number);
-//             var list_item1 = list_item0
-//                 .replace("#shoufa#",dataSource.data[i].start);
-//             var list_item2 = list_item1
-//                 .replace('#moban#',dataSource.data[i].end);
-//             $(".table-container").append(list_item2);
-//         }
-//     }
-//     else
-//     {
-//         alert(dataSource.code);
-//     }
-// });
+
 var FetchData = function (data, method, param, async) {
     var response =
         $.ajax({
@@ -28,13 +10,20 @@ var FetchData = function (data, method, param, async) {
             success: function (dataSource) {
                 return dataSource;
             }});
-    return response.responseJSON;
+    var result = response.responseJSON.data;
+    for(var i = 0 ; i < result.length ; i++)
+    {
+        result[i].line = result[i].line.replace(/\n/g,'<br/>');
+        console.log(result[i].line.replace('\n','<br/>'));
+    }
+    return result;
 };
+
 $(function () {
     var vm = new Vue({
         el:'#app',
         data: {
-            data1 : FetchData(null, 'POST','/web/getBus',false).data
+            data1 : FetchData(null, 'POST','/web/getBus',false)
         },
         methods: {
 
