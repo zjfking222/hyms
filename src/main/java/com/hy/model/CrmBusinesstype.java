1e56d6a5024cc95c5cacf0e3bafa55d4727a7789
bunnyhsu <179346696@qq.com>
2018-06-27 10:09:10 +0800

    /

---
+++ src/main/java/com/hy/model/CrmBusinesstype.java
@@ -0,0 +1,59 @@
+package com.hy.model;
+
+public class CrmBusinesstype {
+
+    private int id;
+    private String name;
+    private int creater;
+    private String created;
+    private int modifier;
+    private String modified;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCreater() {
+        return creater;
+    }
+
+    public void setCreater(int creater) {
+        this.creater = creater;
+    }
+
+    public String getCreated() {
+        return created;
+    }
+
+    public void setCreated(String created) {
+        this.created = created;
+    }
+
+    public int getModifier() {
+        return modifier;
+    }
+
+    public void setModifier(int modifier) {
+        this.modifier = modifier;
+    }
+
+    public String getModified() {
+        return modified;
+    }
+
+    public void setModified(String modified) {
+        this.modified = modified;
+    }
+}
