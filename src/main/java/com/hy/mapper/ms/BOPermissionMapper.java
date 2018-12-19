8e1a0df092a56354cd02aa74519362cc6f57151f
qmjgithub <qmj123abc@163.com>
2018-12-19 09:21:26 +0800

    1、BO报表权限报表权限树相关

--- src/main/java/com/hy/mapper/ms/BOPermissionMapper.java
+++ src/main/java/com/hy/mapper/ms/BOPermissionMapper.java
@@ -1,7 +1,7 @@
 package com.hy.mapper.ms;
 
 import com.hy.model.BOPermission;
-import org.springframework.data.repository.query.Param;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
