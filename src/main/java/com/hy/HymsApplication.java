f6a98212ca1f57ab3201c76c7c06fcd33b67dd00
qmjgithub <qmj123abc@163.com>
2019-01-05 14:36:53 +0800

    1、springboot全局跨域；

--- src/main/java/com/hy/HymsApplication.java
+++ src/main/java/com/hy/HymsApplication.java
@@ -3,7 +3,11 @@ package com.hy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
@@ -11,6 +15,26 @@ import java.util.TimeZone;
 @EnableTransactionManagement
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class HymsApplication {
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.setAllowCredentials(true);//这两句不加不能跨域上传文件，
+        corsConfiguration.setMaxAge(3600l);//加上去就可以了
+        return corsConfiguration;
+    }
+
+    /**
+     * 跨域过滤器
+     * @return
+     */
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", buildConfig()); // 4
+        return new CorsFilter(source);
+    }
     public static void main(String[] args) {
         SpringApplication.run(HymsApplication.class, args);
         System.out.println("启动成功!");
