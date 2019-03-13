2bb9fc3d11cb79cfeb54343737faff2bdf1b3f14
qmjgithub <qmj123abc@163.com>
2019-03-13 09:47:44 +0800

    1、为保证与2.0版本恒拓访问相关页面的一致性做的特殊修改；

--- src/main/java/com/hy/config/shiro/ShiroConfiguration.java
+++ src/main/java/com/hy/config/shiro/ShiroConfiguration.java
@@ -199,6 +199,9 @@ public class ShiroConfiguration {
         filterChainDefinitionMap.put("/m/identity/passwordSuccess.html", "anon");
         //BO报表登录
         filterChainDefinitionMap.put("/bo/index/*", "anon");
+        //恒拓对接开放
+        filterChainDefinitionMap.put("/WPJoint/index/**", "anon");
+        filterChainDefinitionMap.put("/unauthorized/**", "anon");
 
         //<!-- authc:所有url都必须认证通过才可以访问; anon:所有url都都可以匿名访问-->
         //自定义加载权限资源关系
